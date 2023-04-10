beb88896049213b98ec422833d4dc3ec261a0535
Prachi <kothawade.prachi12@gmail.com>
2023-04-10 14:38:34 -0500

    Rest Demo Project

---
+++ ExcerciseOne_Rest/src/main/java/com/SpringBoot/ExcerciseOne_Rest/ExcerciseOneRestApplication.java
@@ -0,0 +1,14 @@
+package com.SpringBoot.ExcerciseOne_Rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.SpringBoot"})
+
+public class ExcerciseOneRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcerciseOneRestApplication.class, args);
+	}
+
+}
